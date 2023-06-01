7ae1ff7ada9dca9f7858bac81036563a3f52a39d
Sahithichittoory <119720163+Sahithichittoory@users.noreply.github.com>
2023-06-01 12:52:54 +0530

    import java.util.*; public class sol {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int m=sc.nextInt();         int s=0;         int a[][]=new int[n][m];         for(int i=0;i<n;i++)         {             for(int j=0;j<m;j++)             {                 a[i][j]=sc.nextInt();             }         }         for(int i=0;i<n;i++)         {             for(int j=0;j<m;j++)             {                 s=s+a[i][j];             }         }         System.out.println(s);     } }

---
+++ Sum_of_matrix_elements.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class sol
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int s=0;
+        int a[][]=new int[n][m];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                a[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                s=s+a[i][j];
+            }
+        }
+        System.out.println(s);
+    }
+}
